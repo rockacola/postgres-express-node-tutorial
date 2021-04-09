82e83a6500d4e5a902f282d0547269b100435c5d
Travis <travis@localhost.dev>
2021-04-09 11:33:17 +1000

    Wrap existing todo test cases into a test suite

--- test/integration/todo.test.js
+++ test/integration/todo.test.js
@@ -2,9 +2,42 @@ const { default: axios } = require("axios");
 
 const baseUrl = "http://localhost:8000/api"; // TODO: move this to a config
 
-test("Show todo list", async () => {
-  const url = baseUrl + "/todos";
-  const res = await axios.get(url);
-  const todos = res.data;
-  expect(todos.length).toBeGreaterThanOrEqual(0);
+describe("CRUD on todo list", () => {
+  let todoListCount;
+  let todoListId;
+
+  test("Show todo list", async () => {
+    const url = baseUrl + "/todos";
+    const res = await axios.get(url);
+    expect(res.status).toBe(200);
+    const todos = res.data;
+    expect(todos.length).toBeGreaterThanOrEqual(0);
+
+    todoListCount = todos.length;
+  });
+
+  test("Create a todo list", async () => {
+    const url = baseUrl + "/todos";
+    const payload = {
+      title: "Dummy todo list",
+    };
+    const res = await axios.post(url, payload);
+    expect(res.status).toBe(201);
+    const todo = res.data;
+    expect(todo.title).toBe(payload.title);
+
+    todoListId = todo.id;
+  });
+
+  test("Count todo list", async () => {
+    const url = baseUrl + "/todos";
+    const res = await axios.get(url);
+    expect(res.status).toBe(200);
+    const todos = res.data;
+    expect(todos.length).toBeGreaterThanOrEqual(todoListCount + 1);
+  });
+
+  // TODO: mod an existing todo list
+
+  // TODO : delete a todo list
 });
